98d90b8291b6502e9c9fa9624f4959c633d27a80
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2023-10-28 07:46:53 +0530

    setup custom middleware info ,error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import app from "./app.js";
+import logger from "./config/logger.config.js";
 
 // ENV variables
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}...`);
+  logger.info(`Server is listening on ${PORT}...`);
 });
