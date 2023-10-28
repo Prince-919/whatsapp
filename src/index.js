33e2924cbab26096b13b72bb5931f4e2f101868b
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2023-10-28 08:31:43 +0530

    uncaughtException or uncaughtException or SIGTERM, handle errors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,34 @@ import logger from "./config/logger.config.js";
 // ENV variables
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+let server = app.listen(PORT, () => {
   logger.info(`Server is listening on ${PORT}...`);
+  console.log("process id", process.pid);
+});
+
+const errorHandle = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  errorHandle();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("uncaughtException", unexpectedErrorHandler);
+
+// SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    if (server) {
+      logger.info("Server closed.");
+      process.exit(1);
+    }
+  }
 });
